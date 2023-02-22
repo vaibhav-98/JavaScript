da1bf54f53c24a9d0e08ada07b9fc41cb6becf90
vaibahv-98 <vaibhavtriapthi1512@gmail.com>
2023-02-22 11:11:34 +0530

    added

---
+++ Array.js
@@ -0,0 +1,9 @@
+let a = [1,2 , "vaibhav" , 52]
+
+console.log (a)
+
+let b = new Array ("abc", "cde ", "aaa")
+console.log(b)
+
+a[1]=5;
+console.log(a)
---
+++ ArrayMethod.js
@@ -0,0 +1,20 @@
+let arr = [1,2,3,4,5]
+
+arr.push(6,7)
+console.log(arr)
+
+arr.pop()
+console.log(arr)
+
+let b = arr.shift ()
+console.log(b)
+console.log(arr)
+arr.unshift(10)
+console.log(arr)
+
+
+let a1= [1,2,3]
+let a2= [4,5,6]
+
+let a3 = a1.concat(a2)
+console.log(a3)
