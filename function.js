cafb131cf88ed5920acb71a0a193b391e6da9b15
vaibahv-98 <vaibhavtriapthi1512@gmail.com>
2023-03-03 18:08:06 +0530

    add code my repo

---
+++ function.js
@@ -0,0 +1,41 @@
+//function 
+
+function sqr (x) { // parameter
+
+    console.log (x*x)
+    return
+}
+
+sqr(8) // argument
+
+
+// Fuction without a parameter and return 
+
+function hello () {
+    console.log("hii")
+}
+
+hello()
+
+///function with a paramter ( single , multiple)
+
+function sum (x,y) {
+    console.log (x+y)
+}
+
+sum (8,2)
+
+// Array as an argument
+
+function sum1 ([a,b,c,d]) {
+
+    console.log(a+b) 
+    console.log (c-d)
+}
+
+sum1([4,5,8,3])
+
+// IIFE & self invoking function 
+
+
+
